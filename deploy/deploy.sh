2e2ae37095cdf72f03c3abf107c07aeee9e8862c
lancelou-new <loucongnew@gmail.com>
2017-12-30 16:39:26 +0800

    fix: auto deploy conf

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
- WEB_PATH='/home/lancelou/www/lanceblog/Blog'
+WEB_PATH='/home/lancelou/www/lanceblog/Blog'
 WEB_USER='lancelou'
 WEB_USERGROUP='lancelou'
  
@@ -10,8 +10,8 @@ cd $WEB_PATH
 echo "pulling source code..."
 git reset --hard origin/master
 git clean -f
-git pull
 git checkout master
+git pull
 echo "Install package && Build package"
 cd front && npm install && npm run build
 cd ..
