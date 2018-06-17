9bb85f5a0385f8f405932d62dbe58914a36bae91
lancelou-new <lincong.lou@gmail.com>
2018-06-17 21:21:20 +0800

    update ci deploy conf

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 mkdir ~/deploy
 cd ~/blog-front
 
-cp -r ~/dist ../deploy
-cp -r ~/config ../deploy
-cp -f ~/middleware ../deploy
-cp -f ~/server ../deploy
-cp ~/index.js ../deploy
-cp ~/server.js ../deploy
-cp ~/pm2.json ../deploy
+cp -r ./dist ../deploy
+cp -r ./config ../deploy
+cp -r ./middleware ../deploy
+cp -r ./server ../deploy
+cp ./index.js ../deploy
+cp ./server.js ../deploy
+cp ./pm2.json ../deploy
 
 scp -r ~/deploy root@45.76.111.33:/home/lancelou/www/lanceblog
