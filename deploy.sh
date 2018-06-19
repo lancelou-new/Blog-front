b6911fd1613d7a8857b48d4631b96ab769396c95
lancelou-new <lincong.lou@gmail.com>
2018-06-19 09:45:55 +0800

    update deploy conf

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ cp -r ./dist ../deploy
 cp -r ./config ../deploy
 cp -r ./middleware ../deploy
 cp -r ./server ../deploy
+cp -r ./build ../deploy
 cp ./index.js ../deploy
 cp ./server.js ../deploy
 cp ./pm2.json ../deploy
@@ -12,4 +13,4 @@ cp ./package.json ../deploy
 
 scp -r ~/deploy root@45.76.111.33:/home/lancelou/www/lanceblog
 
-ssh root@45.76.111.33 "cd /home/lancelou/www/lanceblog/deploy && yarn install "
+ssh root@45.76.111.33 "cd /home/lancelou/www/lanceblog/deploy && yarn install && pm2 start blogfrontend"
